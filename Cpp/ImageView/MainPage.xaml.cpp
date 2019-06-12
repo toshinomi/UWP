42c313f1630779543a0249cdad75f5a6d5c7af6c
達也 鴛海 <tatsuya.oshinomi.0828@gmail.com>
2019-06-12 22:02:38 +0900

    新規作成

---
+++ Cpp/ImageView/MainPage.xaml.cpp
@@ -0,0 +1,56 @@
+﻿//
+// MainPage.xaml.cpp
+// MainPage クラスの実装。
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace ImageView;
+using namespace Concurrency;
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Media::Imaging;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Storage;
+using namespace Windows::Storage::Pickers;
+using namespace Windows::Storage::Streams;
+using namespace Windows::Storage::AccessCache;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+void ImageView::MainPage::OnClickFileSelect(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	auto picker = ref new Windows::Storage::Pickers::FileOpenPicker();
+	picker->SuggestedStartLocation = Windows::Storage::Pickers::PickerLocationId::VideosLibrary;
+	picker->FileTypeFilter->Append("*");
+	picker->FileTypeFilter->Append(".png");
+	picker->FileTypeFilter->Append(".jpg");
+	picker->FileTypeFilter->Append(".jpeg");
+	create_task(picker->PickSingleFileAsync()).then([this](Windows::Storage::StorageFile^ file)
+	{
+		if (file == nullptr)
+		{
+			return;
+		}
+
+		create_task(file->OpenReadAsync()).then([this](Windows::Storage::Streams::IRandomAccessStreamWithContentType^ openFile)
+		{
+			auto bitmap = ref new BitmapImage();
+			bitmap->SetSource(openFile);
+			this->Image->Source = bitmap;
+		});
+	});
+}
---
+++ Cpp/ImageView/MainPage.xaml.h
@@ -0,0 +1,22 @@
+﻿//
+// MainPage.xaml.h
+// MainPage クラスの宣言。
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace ImageView
+{
+	/// <summary>
+	/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+		void OnClickFileSelect(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+	};
+}
