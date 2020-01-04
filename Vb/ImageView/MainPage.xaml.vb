922023214d4cbe53812c08dfb66285d0b70eb7cc
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-01-04 15:06:49 +0900

    Update

--- Vb/ImageView/MainPage.xaml.vb
+++ Vb/ImageView/MainPage.xaml.vb
@@ -1,11 +1,15 @@
 ﻿' 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
+Imports Windows.Storage.Streams
 ''' <summary>
 ''' それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
 ''' </summary>
 Public NotInheritable Class MainPage
     Inherits Page
 
+    ''' <summary>
+    ''' コンストラクタ
+    ''' </summary>
     Public Sub New()
 
         ' この呼び出しはデザイナーで必要です。
@@ -15,6 +19,11 @@ Public NotInheritable Class MainPage
 
     End Sub
 
+    ''' <summary>
+    ''' ファイル選択ボタンのクリックイベント
+    ''' </summary>
+    ''' <param name="sender">オブジェクト</param>
+    ''' <param name="e">ルーティングイベントのデータ</param>
     Public Async Sub OnClickFileSelect(ByVal sender As Object, ByVal e As RoutedEventArgs)
         Dim picker = New Windows.Storage.Pickers.FileOpenPicker()
         picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary
@@ -28,9 +37,17 @@ Public NotInheritable Class MainPage
         End If
 
         Dim openFile = Await file.OpenReadAsync()
-        Dim bitmap As BitmapImage = New BitmapImage()
-        bitmap.SetSource(openFile)
-        Me.Image.Source = bitmap
+        Me.Image.Source = CreateImage(openFile)
     End Sub
 
+    ''' <summary>
+    ''' ビットマップイメージ生成
+    ''' </summary>
+    ''' <param name="_openFile">ファイルランダムアクセスストリーム</param>
+    ''' <returns>ビットマップイメージ</returns>
+    Public Function CreateImage(ByVal _openFile As IRandomAccessStreamWithContentType) As BitmapImage
+        Dim bitmap As BitmapImage = New BitmapImage()
+        bitmap.SetSource(_openFile)
+        Return bitmap
+    End Function
 End Class
