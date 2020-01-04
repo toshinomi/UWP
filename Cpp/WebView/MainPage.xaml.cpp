d74e8ac62301c47814bc0b483c6e0770ce80abaf
toshinomi <tatsuya.oshinomi.0828@gmail.com>
2020-01-04 16:17:22 +0900

    Update

--- Cpp/WebView/MainPage.xaml.cpp
+++ Cpp/WebView/MainPage.xaml.cpp
@@ -22,11 +22,19 @@ using namespace Windows::UI::Xaml::Navigation;
 
 // 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
 
+/// <summary>
+/// コンストラクタ
+/// </summary>
 MainPage::MainPage()
 {
 	InitializeComponent();
 }
 
+/// <summary>
+/// ナビゲーション完了時に発生するイベント
+/// </summary>
+/// <param name="sender">WebView</param>
+/// <param name="e">WebViewナビゲーション完了時イベントのデータ</param>
 void MainPage::OnNavigationCompletedWebView(Windows::UI::Xaml::Controls::WebView^ sender, Windows::UI::Xaml::Controls::WebViewNavigationCompletedEventArgs^ e)
 {
 	textUri->Text = GetCurUri();
@@ -43,6 +51,10 @@ void MainPage::OnNavigationCompletedWebView(Windows::UI::Xaml::Controls::WebView
 	return;
 }
 
+/// <summary>
+/// 遷移先のページがアクティブになる前に呼び出されるイベント
+/// </summary>
+/// <param name="e">ナビゲーションベントのデータ</param>
 void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
 {
 	HomeWebView();
@@ -50,6 +62,11 @@ void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
 	return;
 }
 
+/// <summary>
+/// 戻るボタンのクリックイベント
+/// </summary>
+/// <param name="sender">オブジェクト</param>
+/// <param name="e">ルーティングイベントのデータ</param>
 void MainPage::OnClickBack(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	BackWebView();
@@ -57,6 +74,11 @@ void MainPage::OnClickBack(Platform::Object^ sender, Windows::UI::Xaml::RoutedEv
 	return;
 }
 
+/// <summary>
+/// 進むボタンのクリックイベント
+/// </summary>
+/// <param name="sender">オブジェクト</param>
+/// <param name="e">ルーティングイベントのデータ</param>
 void MainPage::OnClickForward(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	ForwardWebView();
@@ -64,6 +86,11 @@ void MainPage::OnClickForward(Platform::Object^ sender, Windows::UI::Xaml::Route
 	return;
 }
 
+/// <summary>
+/// リフレッシュボタンのクリックイベント
+/// </summary>
+/// <param name="sender">オブジェクト</param>
+/// <param name="e">ルーティングイベントのデータ</param>
 void MainPage::OnClickRefresh(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	RefreshWebView();
@@ -71,6 +98,11 @@ void MainPage::OnClickRefresh(Platform::Object^ sender, Windows::UI::Xaml::Route
 	return;
 }
 
+/// <summary>
+/// ホームボタンのクリックイベント
+/// </summary>
+/// <param name="sender">オブジェクト</param>
+/// <param name="e">ルーティングイベントのデータ</param>
 void MainPage::OnClickHome(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	HomeWebView();
@@ -78,6 +110,11 @@ void MainPage::OnClickHome(Platform::Object^ sender, Windows::UI::Xaml::RoutedEv
 	return;
 }
 
+/// <summary>
+/// サーチボタンのクリックイベント
+/// </summary>
+/// <param name="sender">オブジェクト</param>
+/// <param name="e">ルーティングイベントのデータ</param>
 void MainPage::OnClickFind(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
 {
 	Platform::String^ strUri = textUri->Text;
@@ -86,6 +123,11 @@ void MainPage::OnClickFind(Platform::Object^ sender, Windows::UI::Xaml::RoutedEv
 	return;
 }
 
+/// <summary>
+/// URIテキストボックスのキーダウンイベント
+/// </summary>
+/// <param name="sender">オブジェクト</param>
+/// <param name="e">キールーティングイベントのデータ</param>
 void MainPage::OnKeyDownTextUri(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e)
 {
 	if (e->Key == Windows::System::VirtualKey::Enter)
@@ -97,6 +139,11 @@ void MainPage::OnKeyDownTextUri(Platform::Object^ sender, Windows::UI::Xaml::Inp
 	return;
 }
 
+/// <summary>
+/// WebView表示
+/// </summary>
+/// <param name="_strUri">URI</param>
+/// <returns>WebView表示 成功/失敗</returns>
 bool MainPage::ShowWebView(Platform::String^ _strUri)
 {
 	bool bRst = true;
@@ -113,6 +160,10 @@ bool MainPage::ShowWebView(Platform::String^ _strUri)
 	return bRst;
 }
 
+/// <summary>
+/// WebView戻る
+/// </summary>
+/// <returns>WebView戻る 成功/失敗</returns>
 bool MainPage::BackWebView(void)
 {
 	bool bRst = true;
@@ -128,6 +179,10 @@ bool MainPage::BackWebView(void)
 	return bRst;
 }
 
+/// <summary>
+/// WebView進む
+/// </summary>
+/// <returns>WebView進む 成功/失敗</returns>
 bool MainPage::ForwardWebView(void)
 {
 	bool bRst = true;
@@ -143,6 +198,10 @@ bool MainPage::ForwardWebView(void)
 	return bRst;
 }
 
+/// <summary>
+/// WebViewリフレッシュ
+/// </summary>
+/// <returns>WebViewリフレッシュ 成功/失敗</returns>
 bool MainPage::RefreshWebView(void)
 {
 	bool bRst = true;
@@ -158,6 +217,10 @@ bool MainPage::RefreshWebView(void)
 	return bRst;
 }
 
+/// <summary>
+/// WebViewホーム
+/// </summary>
+/// <returns>WebViewホーム 成功/失敗</returns>
 bool MainPage::HomeWebView(void)
 {
 	textUri->Text = m_strUri;
@@ -165,6 +228,10 @@ bool MainPage::HomeWebView(void)
 	return ShowWebView(strUri);
 }
 
+/// <summary>
+/// URI取得
+/// </summary>
+/// <returns>URI</returns>
 Platform::String^ MainPage::GetCurUri(void)
 {
 	Uri^ uriSource = webView->Source;
--- Cpp/WebView/MainPage.xaml.h
+++ Cpp/WebView/MainPage.xaml.h
@@ -15,27 +15,106 @@ namespace WebView
 	public ref class MainPage sealed
 	{
 	public:
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		MainPage();
+
+		/// <summary>
+		/// WebView表示
+		/// </summary>
+		/// <param name="_strUri">URI</param>
+		/// <returns>WebView表示 成功/失敗</returns>
 		bool ShowWebView(Platform::String^ _strUri);
+
+		/// <summary>
+		/// WebView戻る
+		/// </summary>
+		/// <returns>WebView戻る 成功/失敗</returns>
 		bool BackWebView(void);
+
+		/// <summary>
+		/// WebView進む
+		/// </summary>
+		/// <returns>WebView進む 成功/失敗</returns>
 		bool ForwardWebView(void);
+
+		/// <summary>
+		/// WebViewリフレッシュ
+		/// </summary>
+		/// <returns>WebViewリフレッシュ 成功/失敗</returns>
 		bool RefreshWebView(void);
+
+		/// <summary>
+		/// WebViewホーム
+		/// </summary>
+		/// <returns>WebViewホーム 成功/失敗</returns>
 		bool HomeWebView(void);
+
+		/// <summary>
+		/// URI取得
+		/// </summary>
+		/// <returns>URI</returns>
 		Platform::String^ GetCurUri(void);
 
 	protected:
+		/// <summary>
+		/// 遷移先のページがアクティブになる前に呼び出されるイベント
+		/// </summary>
+		/// <param name="e">ナビゲーションベントのデータ</param>
 		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
 
 	private:
 		Platform::String^ m_strUri = "https://www.bing.com/";
 
 	public:
+		/// <summary>
+		/// ナビゲーション完了時に発生するイベント
+		/// </summary>
+		/// <param name="sender">WebView</param>
+		/// <param name="e">WebViewナビゲーション完了時イベントのデータ</param>
 		void OnNavigationCompletedWebView(Windows::UI::Xaml::Controls::WebView^ sender, Windows::UI::Xaml::Controls::WebViewNavigationCompletedEventArgs^ e);
+
+		/// <summary>
+		/// 戻るボタンのクリックイベント
+		/// </summary>
+		/// <param name="sender">オブジェクト</param>
+		/// <param name="e">ルーティングイベントのデータ</param>
 		void OnClickBack(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		/// <summary>
+		/// 進むボタンのクリックイベント
+		/// </summary>
+		/// <param name="sender">オブジェクト</param>
+		/// <param name="e">ルーティングイベントのデータ</param>
 		void OnClickForward(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		/// <summary>
+		/// リフレッシュボタンのクリックイベント
+		/// </summary>
+		/// <param name="sender">オブジェクト</param>
+		/// <param name="e">ルーティングイベントのデータ</param>
 		void OnClickRefresh(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		/// <summary>
+		/// ホームボタンのクリックイベント
+		/// </summary>
+		/// <param name="sender">オブジェクト</param>
+		/// <param name="e">ルーティングイベントのデータ</param>
 		void OnClickHome(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		/// <summary>
+		/// サーチボタンのクリックイベント
+		/// </summary>
+		/// <param name="sender">オブジェクト</param>
+		/// <param name="e">ルーティングイベントのデータ</param>
 		void OnClickFind(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+
+		/// <summary>
+		/// URIテキストボックスのキーダウンイベント
+		/// </summary>
+		/// <param name="sender">オブジェクト</param>
+		/// <param name="e">キールーティングイベントのデータ</param>
 		void OnKeyDownTextUri(Platform::Object^ sender, Windows::UI::Xaml::Input::KeyRoutedEventArgs^ e);
 	};
 }
